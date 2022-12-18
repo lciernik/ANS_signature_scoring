f59cb7e2dc33d5cc3302efc3ba1b906e6ca05591
lciernik <lciernik@ethz.ch>
2022-12-18 15:55:32 +0100

    changed names

--- signaturescoring/scoring_methods/gene_signature_scoring.py
+++ signaturescoring/scoring_methods/gene_signature_scoring.py
@@ -13,8 +13,8 @@ from signaturescoring.scoring_methods.neighborhood_scoring import \
     score_genes as neighborhood_scoring
 from signaturescoring.scoring_methods.tirosh_ag_scoring import \
     score_genes as tirosh_ag_scoring
-from signaturescoring.scoring_methods.tirosh_lv_scoring import \
-    score_genes as tirosh_lv_scoring
+from signaturescoring.scoring_methods.tirosh_lvg_scoring import \
+    score_genes as tirosh_lvg_scoring
 from signaturescoring.scoring_methods.tirosh_scoring import \
     score_genes as tirosh_scoring
 from signaturescoring.scoring_methods.ucell_scoring import \
@@ -27,13 +27,13 @@ def score_signature(adata: AnnData,
                     **kwarg) -> Optional[AnnData]:
     """
     Wrapper method to call one of the available gene expression signature scoring methods (ANS, Tirosh, Triosh_AG,
-    Tirosh_LV, Scanpy, Jasmine, UCell).
+    Tirosh_LVG, Scanpy, Jasmine, UCell).
 
     Args:
         adata: AnnData object containing the gene expression data.
         gene_list: A list of genes,i.e., gene expression signature, for which the cells are scored for.
         method: Scoring method to use. One of ['adjusted_neighborhood_scoring', 'tirosh_scoring', 'tirosh_ag_scoring',
-                'tirosh_lv_scoring', 'scanpy_scoring', 'jasmine_scoring', 'ucell_scoring', 'neighborhood_scoring',
+                'tirosh_lvg_scoring', 'scanpy_scoring', 'jasmine_scoring', 'ucell_scoring', 'neighborhood_scoring',
                 'corrected_scanpy_scoring']
         **kwarg: Other keyword arguments specific for the scoring method. See below names of individual scoring methods
                  and their available keyword arguments.
@@ -47,9 +47,9 @@ def score_signature(adata: AnnData,
             (see signaturescoring.scoring_methods.adjusted_neighborhood_scoring.score_genes)
         Tirosh: Scoring method based on approach suggested by Tirosh et al. 2016 (10.1126/science.aad0501).
             (see signaturescoring.scoring_methods.tirosh_scoring.score_genes)
-        Tiosh_AG, Tirosh_LV: Modifications of above method. First selecting all genes in an expression bin as control
+        Tiosh_AG, Tirosh_LVG: Modifications of above method. First selecting all genes in an expression bin as control
                              genes. Latter selecting the least variable genes of an expression bin as control genes.
-                             (see signaturescoring.scoring_methods.tirosh_[ag/lv]_scoring.score_genes)
+                             (see signaturescoring.scoring_methods.tirosh_[ag/lvg]_scoring.score_genes)
         Scanpy: Scoring method implemented in Scanpy
                 (https://scanpy.readthedocs.io/en/stable/generated/scanpy.tl.score_genes.html)
         Jasmine: Rank-based signature scoring method by Noureen et al. 2022 (https://doi.org/10.7554/eLife.71994)
@@ -63,8 +63,8 @@ def score_signature(adata: AnnData,
         return tirosh_scoring(adata, gene_list, **kwarg)
     elif method == "tirosh_ag_scoring":
         return tirosh_ag_scoring(adata, gene_list, **kwarg)
-    elif method == "tirosh_lv_scoring":
-        return tirosh_lv_scoring(adata, gene_list, **kwarg)
+    elif method == "tirosh_lvg_scoring":
+        return tirosh_lvg_scoring(adata, gene_list, **kwarg)
     elif method == "scanpy_scoring":
         return scanpy_scoring(adata, gene_list, **kwarg)
     elif method == "jasmine_scoring":
@@ -79,7 +79,7 @@ def score_signature(adata: AnnData,
         msg = f""" 
         Unknown gene scoring method {method}. 
         Choose between ['adjusted_neighborhood_scoring', 'tirosh_scoring', 'tirosh_ag_scoring', 
-                        'tirosh_lv_scoring', 'scanpy_scoring', 'jasmine_scoring', 'ucell_scoring', 
+                        'tirosh_lvg_scoring', 'scanpy_scoring', 'jasmine_scoring', 'ucell_scoring', 
                         'neighborhood_scoring', 'corrected_scanpy_scoring']
         """
         raise ValueError(msg)
